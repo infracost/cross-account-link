c47f58c13ab07ac99f883eb3251b4c27a61cfca2
Alistair Scott <ali.scott@gmail.com>
2025-03-03 14:03:13 +0000

    enhance: simplify permissions (#4)
    Only use the permissions we need for Compute Optimizer and the services we get from it

--- variables.tf
+++ variables.tf
@@ -8,15 +8,3 @@ variable "infracost_account" {
   type        = string
   default     = "237144093413"
 }
-
-variable "include_cloudwatch_readonly_policy" {
-  description = "Whether to include the CloudWatch read-only policy in the Cross-Account Role."
-  type        = bool
-  default     = true
-}
-
-variable "include_services_readonly_policy" {
-  description = "Whether to include the services read-only policy in the Cross-Account Role."
-  type        = bool
-  default     = true
-}
