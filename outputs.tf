0b27a821f428821600f6c5234f2c0219a00e88ce
Sean Pomeroy <sean.pomeroy@infracost.io>
2026-05-22 17:09:09 -0400

    docs(outputs): remove 'Provide this to Infracost' from output descriptions

--- outputs.tf
+++ outputs.tf
@@ -9,26 +9,26 @@ output "account_region" {
 }
 
 output "role_arn" {
-  description = "The ARN value of the Cross-Account Role with IAM read-only permissions. Provide this to Infracost."
+  description = "The ARN value of the Cross-Account Role with IAM read-only permissions."
   value       = aws_iam_role.cross_account_role.arn
 }
 
 output "billing_and_cost_management_export_arn" {
-  description = "The ARN of the BCM Data Exports export. Provide this to Infracost."
+  description = "The ARN of the BCM Data Exports export."
   value       = var.enable_data_exports ? module.billing_and_cost_management_export[0].export_arn : null
 }
 
 output "billing_and_cost_management_bucket_arn" {
-  description = "The ARN of the S3 bucket used for BCM Data Exports. Provide this to Infracost."
+  description = "The ARN of the S3 bucket used for BCM Data Exports."
   value       = var.enable_data_exports ? module.billing_and_cost_management_export[0].bucket_arn : null
 }
 
 output "s3_storage_lens_configuration_arn" {
-  description = "The ARN of the S3 Storage Lens configuration. Provide this to Infracost."
+  description = "The ARN of the S3 Storage Lens configuration."
   value       = var.enable_data_exports ? module.s3_storage_lens_export[0].configuration_arn : null
 }
 
 output "s3_storage_lens_bucket_arn" {
-  description = "The ARN of the S3 bucket used for S3 Storage Lens exports. Provide this to Infracost."
+  description = "The ARN of the S3 bucket used for S3 Storage Lens exports."
   value       = var.enable_data_exports ? module.s3_storage_lens_export[0].bucket_arn : null
 }
